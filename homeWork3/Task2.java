4554e56432cfaf431a9eee33549494c271c675d3
Lexan4ik1991 <lexan4ik.pavloff@yandex.ru>
2023-02-02 21:20:11 +0300

    homeWork3.java

--- homeWork3/Task2.java
+++ homeWork3/Task2.java
@@ -13,6 +13,7 @@ public class Task2 {
 
 
         fillArray();
+
     }
 
     public static void fillArray() {
@@ -24,35 +25,37 @@ public class Task2 {
             myArray[i] = random.nextInt(10);
 
         }
-            System.out.println(Arrays.toString(myArray));
-
-            for (int i = 0; i < myArray.length; i++) {
-                aList.add(i, (int) (Math.random() * 10 + 1));
+        System.out.println(Arrays.toString(myArray));
 
 
-            }
-            System.out.println("Unsorted ArrayList "+aList);
+        for (int i = 0; i < myArray.length; i++) {
+            aList.add(i, (int) (Math.random() * 10 + 1));
+        }
+        System.out.println("Unsorted ArrayList " + aList);
         Collections.sort(aList);
         System.out.println("Sorted ArrayList " + aList);
         int max = aList.get(aList.size() - 1);
         int min = aList.get(0);
         System.out.println(min);
         System.out.println(max);
-        private static double getAverage(); {
-            long sum = 0;
-            for (int i: aList) {
-                sum += i;
-            }
-            return aList.size() > 0 ? (double) sum / aList.size() : 0;
-        double avg = getAverage(aList);
-        System.out.println(avg);
+        double sum = 0;
+        double result = 0;
+        for (int i = 0; i < aList.size(); i++) {
+            sum += aList.get(i);
+            result = sum / aList.size();
 
-
-
+        }
+        System.out.println(result);
     }
-
-
 }
+
+
+
+
+
+
+
+
 
 
 
