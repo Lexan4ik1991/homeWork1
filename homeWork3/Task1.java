4e88b6dd691b3b00770e72c8e2baa6f9f4c521d4
Lexan4ik1991 <lexan4ik.pavloff@yandex.ru>
2023-02-06 18:42:37 +0300

    Исправил вывод макс,мин,ср.арифметическогозначений с помощью класса IntSummaryStatistics

--- homeWork3/Task1.java
+++ homeWork3/Task1.java
@@ -2,35 +2,31 @@ package homeWork3;
 
 import java.util.*;
 
+import static java.lang.System.*;
+
 //Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)
 public class Task1 {
     public static void main(String[] args) {
+
         fillArray();
     }
-        public static void fillArray() {
-            List<Integer> aList = new ArrayList<>();
-            int[] myArray = new int[10];
-            Random random = new Random();
-            for (int i = 0; i < myArray.length; i++) {
-                myArray[i] = random.nextInt(10);
 
-            }
-            System.out.println(Arrays.toString(myArray));
-
-            for (int i = 0; i < myArray.length; i++) {
-                aList.add(i, (int) (Math.random() * 10 + 1));
-
-
-            }
-            System.out.println(aList);
-
-        for (Iterator<Integer> iterator = aList.iterator(); iterator.hasNext();) {
-            Integer number = iterator.next();
-            if (number % 2 == 0) {
-                System.out.println(number);
-                iterator.remove();
-            }
+    public static void fillArray() {
+        Random rnd = new Random();
+        ArrayList<Integer> aList = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int num = rnd.nextInt(-10, 10);
+            aList.add(num);
         }
+        System.out.println(aList);
+        for (Iterator<Integer> iterator = aList.iterator(); iterator.hasNext(); ) {
+                Integer number = iterator.next();
+                if (number % 2 == 0) {
+                    System.out.println(number);
+                    iterator.remove();
+                }
+            }
 
         }
-}
+    }
+
