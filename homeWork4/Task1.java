6b919d266d12573f8799b5c63d61e22fa626b72b
Lexan4ik1991 <lexan4ik.pavloff@yandex.ru>
2023-02-09 12:03:06 +0300

    Задача №1 решена через ArrayDeque

--- homeWork4/Task1.java
+++ homeWork4/Task1.java
@@ -1,50 +1,50 @@
 package homeWork4;
-import java.lang.reflect.Array;
+
+import java.lang.constant.Constable;
 import java.util.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 //. Реализовать консольное приложение, которое:
 //Принимает от пользователя и “запоминает” строки.
 //Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
 //Если введено revert, удаляет предыдущую введенную строку из памяти.
 public class Task1 {
+
     public static void main(String[] args) {
-        ArrayList<String> alist = new ArrayList<>();
-        while(true){
+        ArrayDeque<String> dequeArray = new ArrayDeque<>();
+        while (true) {
             String msg = scanerText();
-            if (msg.equalsIgnoreCase("print")){
-                for (int i=alist.size()-1; i>=0;i--){
-                    System.out.println(alist.get(i));
+            if (msg.equalsIgnoreCase("print")) {
+                for (String s : dequeArray) {
+                    System.out.println(s);
                 }
 
-            }if (msg.equalsIgnoreCase("revert")){
-                alist.remove(alist.size()-2);
+            }else if (msg.equalsIgnoreCase("revert")) {
+                    dequeArray.removeFirst();
+                System.out.println(dequeArray);
 
-            }if (msg.equalsIgnoreCase("exit")){
+            }else if (msg.equalsIgnoreCase("exit")) {
                 break;
 
-            }else {
-                alist.add(msg);
-                System.out.println(alist);
+            }else{
+                    dequeArray.addFirst(msg);
+                System.out.println(dequeArray);
 
+
+                }
             }
         }
 
-
-
-    }
-    private static String scanerText(){
-        String msg="";
+        private static String scanerText() {
+        String msg = "";
         Scanner lineScanner = new Scanner(System.in);
         System.out.println("Введите текст");
-        try{
+        try {
             msg = lineScanner.nextLine();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return msg;
+
     }
 }
